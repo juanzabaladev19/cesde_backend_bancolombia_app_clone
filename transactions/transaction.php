a8c43b1354c1a790028636e0939732cc9351356a
juanzabaladev19 <juanzabaladev@gmail.com>
2022-04-18 20:36:39 -0500

    fix list transactons

--- transactions/transaction.php
+++ transactions/transaction.php
@@ -2,12 +2,12 @@
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     require_once(dirname(__DIR__) . "/db/db_connection.php");
-
     try {
         $db = new DBConnection();
         $dbconnection  = $db->connect();
         $data = json_decode(file_get_contents('php://input'), true);
-        $query = "SELECT * FROM transacciones.movimientos ORDER BY transactionNumber DESC LIMIT 10";
+        $pageCount = $data["pageCount"];
+        $query = "SELECT * FROM bancolombia_transactional.transactions ORDER BY transaction_number LIMIT ${pageCount},20";
         $sql = $dbconnection->query($query)->fetchAll(PDO::FETCH_ASSOC);
         header('Content-Type: application/json');
         echo (json_encode($sql));
